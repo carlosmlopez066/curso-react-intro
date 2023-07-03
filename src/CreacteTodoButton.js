208bf51f3883f3e9c2736ec889b4083281be80ff
Carlos Lopez <carlosmlopez066@gmail.com>
2023-07-03 06:00:36 -0400

    onclick nad onChange

--- src/CreacteTodoButton.js
+++ src/CreacteTodoButton.js
@@ -1,7 +1,15 @@
 import "./CreacteTodoButton.css"
 function CreateTodoButton() {
   return (
-    <button>+</button>
+    <button
+      onClick={
+        (event) => {
+          console.log("le diste click al boton")
+          console.log(event)
+          console.log(event.target)
+        }
+      }
+    >+</button>
   );
 }
 
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,9 +1,21 @@
-import React from "react"
+import React, { useState } from "react"
 import "./TodoSearch.css"
 function TodoSearch() {
+
+  const [searchValue, setSearchValue] = React.useState("");
+
+  console.log('usuario:' + searchValue)
+
   return (
     <div className="TodoSearch-container">
-      <input placeholder="Salir a correr" />
+      <input
+        placeholder="Salir a correr"
+        value={searchValue}
+        onChange={(event) => {
+          setSearchValue(event.target.value)
+        }
+        }
+      />
 
     </div>
   )
